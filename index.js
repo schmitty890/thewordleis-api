abfa9758d8498eff37a0ebd7ea85804d386cdd18
Jason Schmitt <schmitty890@gmail.com>
2022-03-05 12:02:39 -1000

    adding express to test on digital ocean

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 import express from "express";
+import bodyParser from "body-parser";
 
-console.log("index.js");
+const PORT = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8081;
+
+const app = express();
+
+// body parser setup
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// serving static files
+app.use(express.static("public"));
+
+app.get("/test", (req, res) => {
+  res.send("test route!");
+});
+
+app.get("/", (req, res) => {
+  console.log(`hi on port ${PORT}`);
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
